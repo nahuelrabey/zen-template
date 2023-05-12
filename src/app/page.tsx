0ae28deef8b727a1eab6e31b3b702fd583b5f1a1
Nahuel Rabey <nahuelrabeywork@gmail.com>
2023-05-12 15:12:11 -0300

    og-image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,13 @@ import Header from "@/components/Header";
 import Headline from "@/components/Headline";
 import Service from "@/components/Services";
 import { Footer } from "@/components/Footer";
+import Head from "next/head";
 export default function Home() {
   return (
     <>
+    <Head>
+      <meta property="og:image" content="https://i.ibb.co/WPCj1rT/Create-Next-App.png" />
+    </Head>
       <Header/>
       <main>
         <Headline/>
