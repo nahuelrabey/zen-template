21fcd0c6a07a04b0eafc0c7508a4e95e446b9ac1
Nahuel Rabey <nahuelrabeywork@gmail.com>
2023-05-12 15:35:47 -0300

    ogimage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,20 @@ import Headline from "@/components/Headline";
 import Service from "@/components/Services";
 import { Footer } from "@/components/Footer";
 import Head from "next/head";
+import type { Metadata } from 'next';
+ 
+
+export const metadata: Metadata = {
+  title: 'Home',
+  description: 'Welcome to Next.js',
+  openGraph:{
+    images:["https://i.ibb.co/WPCj1rT/Create-Next-App.png"]
+  }
+};
+ 
 export default function Home() {
   return (
     <>
-      <Head>
-        <meta
-          property="og:image"
-          content="https://i.ibb.co/WPCj1rT/Create-Next-App.png"
-        />
-      </Head>
       <Header />
       <main>
         <Headline />
