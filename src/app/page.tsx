a7a752061b9fb5b65994e58830518be493e66dfc
Nahuel Rabey <nahuelrabeywork@gmail.com>
2023-05-12 15:04:26 -0300

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 import Header from "@/components/Header";
-import style from "./page.module.scss";
-import Image from "next/image";
 import Headline from "@/components/Headline";
 import Service from "@/components/Services";
+import { Footer } from "@/components/Footer";
 export default function Home() {
   return (
     <>
@@ -11,6 +10,7 @@ export default function Home() {
         <Headline/>
         <Service/>
       </main>
+      <Footer/>
     </>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import style from "./style.module.scss";
+export function Footer() {
+  return (
+    <footer className={style.footer}>
+      <div className={style.content}>
+        <p>
+          &copy; created by <a href="nahuelrabey.com" target="_blank">Nahuel Rabey</a>
+        </p>
+        <p>
+          &#x2192; <a className={style.repo} href="https://github.com/nahuelrabey/zen-template"target="_blank">github repo</a>
+        </p>
+      </div>
+    </footer>
+  );
+}
